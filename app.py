5292cb80b06a563ab00a69a32b3423a0e2184c70
Jason Hanrahan <jasonhanr@gmail.com>
2020-05-05 16:26:43 -0400

    gameboard and user guess form properly displaying on game_started.html

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from boggle import Boggle
 
+# key names will use to store some things in the session;
+# put here as constants so we're guaranteed to be consistent in our spelling of these
+
+GAME_BOARD_SESSION = 'gameboard'
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -12,5 +17,10 @@ debug = DebugToolbarExtension(app)
 boggle_game = Boggle()
 
 @app.route("/")
-def game_start():
-    return render_template("/game-start.html")
+def home_page():
+    return render_template("/home_page.html")
+
+@app.route("/game_started")
+def play_game():
+    session[GAME_BOARD_SESSION] = boggle_game.make_board()
+    return render_template("/game_started.html")
